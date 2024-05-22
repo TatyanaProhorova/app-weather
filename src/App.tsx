9431cc9090523681efe89f0767345cbee3340c08
Tatyana Prohorova <tatyanaprohorova@gmail.com>
2024-05-22 23:01:43 +0300

    css

--- src/App.tsx
+++ src/App.tsx
@@ -1,104 +1,98 @@
-import { ChangeEvent, FormEvent, useState, useEffect } from 'react';
+﻿import { ChangeEvent, FormEvent, useState, useEffect } from 'react';
 import useWeatherData from "./hooks/useWeatherData";
 import './App.css';
-import ForecastCard from './components/ForecastCard';
+import ForecastCard from './components/ForecastCard/ForecastCard';
 
 function App() {
-  const { data, forecast, isLoading, fetchData } = useWeatherData(); // вызвали польз хук;
+  const { data, forecast, isLoading, fetchData } = useWeatherData();
+
+  // if (forecast) {
+  // for (const i of forecast) forecast // Type 'ForecastDays' must have a '[Symbol.iterator]()' method that returns an iterator.
+
+  // if (forecast) {
+  //   const forecast3Days = forecast.filter((key) => {forecast[key].dt_txt !== forecast[0].dt_txt});
+  // }
 
   const [cityName, setCityName] = useState('');
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {     //  event:FormEvent
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {  
     event.preventDefault();
     fetchData({ name: cityName });
     setCityName('');
-    // console.dir(evt.target);
   }
+
+  const currentDate = data ? new Date(data.dt * 1000).toDateString() : new Date().toDateString()
 
   const handleChangeCityName = (event: ChangeEvent<HTMLInputElement>) => {
       setCityName(event.target.value);
   }
 
   useEffect(() => {
-    fetchData({});   
+    fetchData({});
   }, []);
-
-  // interface Item {                   
-  //     dt_txt: string;
-  //     main: {
-  //         feels_like: number;
-  //         temp: number;
-  //     }
-  // } 
-  // const list: Array<{Item}> 
-
-  // const list: Array<{Item}>
-
- const getItems = (itemOfList_0: Array<{                       
-    dt_txt: string;
-    main: {
-      feels_like: number;
-      temp: number;
-    }}> | undefined) => {
-      itemOfList_0 = forecast?.list[0];
-      console.log(forecast?.list[0]);
-    }
- 
-  // const days = []; days[0] = itemOflist?[8]; days[1] = itemOflist?[16];  ///!!!!!!!!!!!!!!!!!!!!!!!!!
-  // console.log('itemOfList --',itemOfList_0); 
-  // console.log('days', days);   
 
   return (
     <>
       <div className="info_panel">
-        <form name="myForm" onSubmit={handleSubmit}>
-            <input type="text" value={cityName} onChange={handleChangeCityName}
-            className="input_field"  placeholder="Введите название города" />
-            <button type="submit">Поиск</button>              
+        <form 
+          name="myForm"
+          className="search-form" 
+          onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={cityName}
+              onChange={handleChangeCityName}
+              className="input_field"
+              placeholder="Введите название города"
+            />
+            <button
+              className='search-button'
+              type="submit">Поиск
+            </button>
         </form>
 
         {isLoading ? (
             <div>Загрузка...</div>
         ) : (
               <>
-                <div className='city_name'>
-                  {data?.name}
-                </div>
-                      
-                <div className="current_date">
-                  <span>Thursday</span><span>month_and_day</span><span>2024</span>
-                  {/* <span>{day_of_week}  </span><span>{month_and_day}  </span><span>{year}</span> */}
-                </div>
-
-                <div>сегодня {forecast?.list[0].dt_txt}</div>
-
-                <div className='temp'>
-                  <span>{data?.main?.temp.toFixed(1)}</span><span>&deg;C</span>
-                </div>
-                          
-                <div className='forecast'>
-                  <div>
-                    <span>прогноз погоды на 3 дня</span>
-
-                    </div> 
-                  <div>                
-                    <ForecastCard day={forecast?.list[8].dt_txt} temp={forecast?.list[8].main.temp}/>  
-                    <ForecastCard day={forecast?.list[16].dt_txt} temp={forecast?.list[16].main.temp}/>
-                    <ForecastCard day={forecast?.list[24].dt_txt} temp={forecast?.list[24].main.temp}/>
+                <div className='city-info'>
+                  <div className='city_name'>
+                    {data?.name}
                   </div>
-                </div>                 
+
+                  <div className="current_date">
+                    {currentDate && (
+                      <div>сегодня {currentDate}</div>
+                    )}
+                  </div>
+
+                  <div className='temp'>
+                    <span>{data?.main?.temp.toFixed(1)}</span><span>&deg;C</span>
+                  </div>
+
+                  <div className='forecast'>
+                    <div>
+                      <span>прогноз погоды</span>
+                    </div>
+                    <div className='forecast__daily'>
+
+                      {forecast ? Object.keys(forecast).map((key) => {
+                          return (
+                              <ForecastCard key={key} day={forecast[key].dt_txt} hours={forecast[key].hours}/>
+                          )
+                      }) : (
+                          <span>Данных о прогнозе погоды нет</span>
+                      )}
+
+                    </div>
+                  </div>
+                </div>
               </>
-            ) 
-        }  
-      </div>  
+            )
+        }
+      </div>
     </>
   )
 }
 
 export default App
-
-
-                      {/* {days.map((day, index) => {
-                        return(
-                          <p key={index}>{day.dt_txt}</p>
-                        )})} */}
---
+++ src/components/ForecastCard/ForecastCard.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+import { ForecastDayHour } from "../../utils/types";
+
+import "./ForecastCard.css";
+
+interface ForecastProps {
+  
+  day: string | null;
+  hours: ForecastDayHour[];
+}
+
+function ForecastCard(props: ForecastProps) {
+const { day, hours} = props;
+
+const dayOfMonth = day? new Date(day).getDate(): undefined;
+const monthName = day? new Date(day).toLocaleString('default', { month: 'long' }): undefined;
+const year = day? new Date(day).getFullYear(): undefined;
+
+  const [isOpen, setOpen] = useState(false);
+  return(
+      <div className='foreCast__card'>
+
+        <button className="foreCast__button" onClick={() => setOpen(!isOpen)}>
+          <div>{dayOfMonth}</div>
+          <div>{monthName}</div>
+          <div>{year}</div>
+        </button>
+
+        <ul className={isOpen ? "dropdown-active" : "dropdown"}>
+          {hours.map((hourItem) => (
+              <li className="dropdown__item">
+                  <div>{`${hourItem.hour}:00`}</div>
+                  <div><span>{hourItem.temp.toFixed(0)}&deg;C</span></div>
+              </li>        
+          ))}
+        </ul>
+      </div> 
+  )
+}
+
+
+export default ForecastCard
